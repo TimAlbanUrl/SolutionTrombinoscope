f92f88acccb6af617dbbfeab0dd848601689b42a
sirsoliloque <victorfriboulet07@gmail.com>
2024-03-07 17:24:48 +0100

    build: connexion chain

--- DllbddPersonnels/BddPersonnels.cs
+++ DllbddPersonnels/BddPersonnels.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using BddpersonnelContext;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,10 @@ namespace DllbddPersonnels
 {
     public class BddPersonnels
     {
+        public BddpersonnelDataContext bdd;
+        public BddPersonnels(String user, String mdp, String serveurIp) {
+            bdd = new BddpersonnelDataContext("User Id=" + user + ";Password=" + mdp + ";Host=" + serveurIp + ";Port=3306;Database=bddpersonnels;Persist Security Info=True");
+        }
+       
     }
 }
