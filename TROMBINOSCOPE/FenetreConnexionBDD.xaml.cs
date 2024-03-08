20f26d366313fa4a800a6f6414e26eb8885190d3
Victor Friboulet <victor.friboulet@etu.unicaen.fr>
2024-03-08 11:47:39 +0100

    fin page connexion

--- TROMBINOSCOPE/FenetreConnexionBDD.xaml.cs
+++ TROMBINOSCOPE/FenetreConnexionBDD.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace AppTrombinoscope
@@ -17,16 +18,42 @@ namespace AppTrombinoscope
     /// <summary>
     /// Logique d'interaction pour FenetreConnexionBDD.xaml
     /// </summary>
-    public partial class FenetreConnexionBDD : Window
+    public partial class FenetreConnexionBDD : UserControl
     {
         public FenetreConnexionBDD()
         {
             InitializeComponent();
         }
+        public string username
+        {
+            get { return ChampUtilisateur.Text; }
+            set { ChampUtilisateur.Text = value; }
+        }
+        public string adress
+        {
+            get { return ChampIP.Text; }
+            set { ChampIP.Text = value; }
+        }
+        public string port
+        {
+            get { return ChampPort.Text; }
+            set { ChampPort.Text = value; }
+        }
+        public string password
+        {
+            get { return ChampMotDePasse.Password; }
+            set { ChampMotDePasse.Password = value; }
+        }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        private void Button_Cancel_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Button_Save_Click(object sender, RoutedEventArgs e)
+        {
+            AppCache.bdd = new DllbddPersonnels.BddPersonnels(username, password, adress, port);
         }
     }
 }
