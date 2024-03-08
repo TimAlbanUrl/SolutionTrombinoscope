c8f08f72c54b32b1e89d6332edb023a68e818241
Victor Friboulet <victor.friboulet@etu.unicaen.fr>
2024-03-08 11:03:44 +0100

    :ui page de connexion à la bdd

---
+++ TROMBINOSCOPE/FenetreConnexionBDD.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppTrombinoscope
+{
+    /// <summary>
+    /// Logique d'interaction pour FenetreConnexionBDD.xaml
+    /// </summary>
+    public partial class FenetreConnexionBDD : Window
+    {
+        public FenetreConnexionBDD()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
