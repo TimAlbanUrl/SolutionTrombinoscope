d4b5b699975baed89d14ccfef17a2be62e44e84f
martin <22201275@etu.unicaen.fr>
2024-03-21 15:49:27 +0100

    build: image filling

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -150,6 +151,28 @@ namespace WpfApp1
 
             serviceField.Text = bdd.GetServiceFromId(personnel.IdService).Intitule;
             functionField.Text = bdd.GetFonctionFromId(personnel.IdFonction).Intitule;
+
+            if(personnel.Photo != null)
+            {
+                imagePreview.Source = ByteToImage(personnel.Photo);
+            } else
+            {
+                BitmapImage image = new BitmapImage(new Uri("./default-avatar-icon.jpg", UriKind.Relative));
+                imagePreview.Source = image;
+            }
+        }
+
+        public static ImageSource ByteToImage(byte[] imageData)
+        {
+            BitmapImage biImg = new BitmapImage();
+            MemoryStream ms = new MemoryStream(imageData);
+            biImg.BeginInit();
+            biImg.StreamSource = ms;
+            biImg.EndInit();
+
+            ImageSource imgSrc = biImg as ImageSource;
+
+            return imgSrc;
         }
     }
 }
